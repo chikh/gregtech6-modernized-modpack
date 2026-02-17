cf9ff7bcd1c6a75c8e7dadc8a5688b0489dde635
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2026-02-17 11:42:32 +0100

    Fix build_server.sh: copy all files to build dir to satisfy packwiz index requirements

--- build_server.sh
+++ build_server.sh
@@ -27,32 +27,17 @@ mkdir -p "$DIST_DIR"
 echo "### Refreshing packwiz index..."
 "$PACKWIZ_BINARY" refresh
 
-echo "### Preparing server distribution files..."
-# Copy necessary files to the temp build dir
-# We use a list to avoid copying junk
-FILES_TO_COPY=(
-    "config"
-    "mods"
-    "serverutilities"
-    "java9args.txt"
-    "pack.toml"
-    "index.toml"
-    "start.sh"
-    "start.bat"
-)
-
-for file in "${FILES_TO_COPY[@]}"; do
-    if [ -e "$file" ]; then
-        cp -r "$file" "$DIST_DIR/"
-    fi
-done
+echo "### Preparing server distribution files (copying everything to avoid FileNotFound errors)..."
+# We copy everything because packwiz index.toml tracks all files in the root.
+# packwiz-installer will fail if it can't find a file listed in the index.
+cp -r . "$DIST_DIR/"
 
 echo "### Downloading Forge Installer..."
 curl -Ls "$FORGE_INSTALLER_URL" -o "$TEMP_BUILD_DIR/forge-installer.jar"
 
 echo "### Running Forge Installer (this may take a minute)..."
-# Run installer inside the temp build directory to isolate library generation
 cd "$DIST_DIR"
+# Install into the current dir (which is DIST_DIR)
 java -jar "$TEMP_BUILD_DIR/forge-installer.jar" --installServer > /dev/null
 cd "$PROJECT_ROOT"
 
@@ -61,22 +46,27 @@ curl -Ls "$BOOTSTRAP_URL" -o "$TEMP_BUILD_DIR/packwiz-installer-bootstrap.jar"
 
 echo "### Downloading all mod JARs (Server-side)..."
 cd "$DIST_DIR"
-# Run the installer. It will download JARs into the mods/ folder using the temp metadata
+# Run the installer. It will download JARs into the mods/ folder.
 java -jar "$TEMP_BUILD_DIR/packwiz-installer-bootstrap.jar" --side server -no-gui "pack.toml"
 
-echo "### Cleaning up metadata and unnecessary files..."
+echo "### Cleaning up metadata and development files from the bundle..."
+# Remove packwiz metadata
 rm pack.toml index.toml
 find mods -name "*.pw.toml" -delete
+# Remove build scripts, hidden files, and repo-only files
+rm -rf build_server.sh AI_CONTEXT.md .git .gitignore logo.png icon.png docker-compose.yml
+cd -
 
 echo "### Downloading lwjgl3ify-forgePatches..."
-curl -Ls "$FORGE_PATCHES_URL" -o "lwjgl3ify-forgePatches.jar"
+curl -Ls "$FORGE_PATCHES_URL" -o "$DIST_DIR/lwjgl3ify-forgePatches.jar"
 
 echo "### Bundling into $ZIP_NAME..."
+cd "$DIST_DIR"
 zip -rq "$ZIP_NAME" .
 mv "$ZIP_NAME" "$ZIP_TARGET"
 cd "$PROJECT_ROOT"
 
-echo "### Cleaning up temporary files..."
+echo "### Cleaning up temporary build files..."
 rm -rf "$TEMP_BUILD_DIR"
 
 echo "### Done! Server bundle is ready at: $ZIP_TARGET"
